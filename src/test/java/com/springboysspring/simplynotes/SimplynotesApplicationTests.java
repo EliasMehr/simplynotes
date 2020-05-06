205b0a67ccf2e457613a5350f720a7f8cef601d8
eliasmehr <Falafel123!>
2020-05-06 14:06:04 +0200

    Initial commit

---
+++ src/test/java/com/springboysspring/simplynotes/SimplynotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboysspring.simplynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplynotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
