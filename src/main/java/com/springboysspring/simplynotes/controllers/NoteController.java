a2ad8be471c06b3db612bd390c6cf71e459b55da
Johannes Svensson <johannes.svensson97@gmail.com>
2020-05-12 15:49:20 +0200

    NoteController + NoteService created

---
+++ src/main/java/com/springboysspring/simplynotes/controllers/NoteController.java
@@ -0,0 +1,87 @@
+package com.springboysspring.simplynotes.controllers;
+
+import com.springboysspring.simplynotes.models.Note;
+import com.springboysspring.simplynotes.services.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class NoteController {
+
+    private NoteService noteService;
+    @Autowired
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/notes/user/{id}")
+    public ResponseEntity<List<Note>> getNotesByUserID(@PathVariable UUID id) {
+        List<Note> response;
+        try {
+            response = noteService.getNotesByUserID(id);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @PostMapping("/notes/user/{id}")
+    public ResponseEntity<String> addNoteToUserId(@PathVariable UUID id, @RequestBody Note note) {
+        try {
+            noteService.addNoteToUserId(id, note);
+            return ResponseEntity.status(HttpStatus.OK).body("Note added to user");
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @PutMapping("/notes/{id}")
+    public ResponseEntity<String> updateNoteById(@PathVariable UUID id, @RequestBody Note note) {
+        try {
+            noteService.updateNoteById(id, note);
+            return ResponseEntity.status(HttpStatus.OK).body("Note updated");
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @DeleteMapping("/notes/{id}")
+    public ResponseEntity<String> deleteNoteById(@PathVariable UUID id) {
+        try {
+            noteService.deleteNoteById(id);
+            return ResponseEntity.status(HttpStatus.OK).body("Note was deleted");
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/notes/{noteId}/user/{userId}")
+    public void sendNoteToFriend(@PathVariable UUID noteId,
+                                 UUID userId,
+                                 @RequestParam String friendName) { // /notes/{noteId}/user/{userId}?friendName=Fazli
+        // find note by id
+        // find user and then his friend == friendName
+        // make new Note with copy users note attr
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/notes/{id}/convert")
+    public void convertNoteAsTodo(@PathVariable UUID id) {
+        // find note by id
+        // copy note, create to.do
+        // delete note
+        // add to.do to db
+    }
+}
---
+++ src/main/java/com/springboysspring/simplynotes/services/NoteService.java
@@ -0,0 +1,70 @@
+package com.springboysspring.simplynotes.services;
+
+import com.springboysspring.simplynotes.models.Note;
+import com.springboysspring.simplynotes.models.User;
+import com.springboysspring.simplynotes.repositories.NoteRepository;
+import com.springboysspring.simplynotes.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class NoteService {
+
+    private NoteRepository noteRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    public NoteService(NoteRepository noteRepository, UserRepository userRepository) {
+        this.noteRepository = noteRepository;
+        this.userRepository = userRepository;
+    }
+
+    public User getUser(UUID id) throws Exception { // Kanske kan vara en metod i userService ist
+        Optional<User> owner = userRepository.findById(id);
+        if (owner.isPresent()) {
+            return owner.get();
+        }
+        throw new Exception("User not Found");
+    }
+
+    public List<Note> getNotesByUserID(UUID id) throws Exception {
+        return noteRepository.findAllByOwner(getUser(id));
+    }
+
+
+    public void addNoteToUserId(UUID id, Note note) throws Exception {
+        getUser(id).addNote(note);
+        try {
+            noteRepository.save(note);
+        } catch (Exception e) {
+            throw new Exception("Could not persist Note to Database");
+        }
+    }
+
+    public void deleteNoteById(UUID id) throws Exception {
+        try {
+            noteRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new Exception("No note with that id exists");
+        }
+    }
+
+    public void updateNoteById(UUID id, Note newNote) throws Exception {
+        Optional<Note> oldNote = noteRepository.findById(id);
+        if (oldNote.isPresent()) {
+            oldNote.get().setTitle(newNote.getTitle());
+            oldNote.get().setContent(newNote.getContent());
+            try {
+                noteRepository.save(oldNote.get());
+            } catch (Exception e) {
+                throw new Exception("Could not persist Note to Database");
+            }
+        } else {
+            throw new Exception("No note with that id exists");
+        }
+    }
+}
