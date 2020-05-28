a107c60808129ca1551d886057a8b6ea66ca8bf4
Johannes Svensson <johannes.svensson97@gmail.com>
2020-05-28 21:11:59 +0200

    updated Cors config

--- src/main/java/com/springboysspring/simplynotes/configuration/CORSConfiguration.java
+++ src/main/java/com/springboysspring/simplynotes/configuration/CORSConfiguration.java
@@ -1,26 +1,26 @@
-//package com.springboysspring.simplynotes.configuration;
-//
-//import javax.validation.constraints.NotNull;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CORSConfiguration {
-//
-//    @NotNull
-//    @Bean
-//    public WebMvcConfigurer CORSConfig() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                    .allowedMethods("GET", "POST", "PUT", "DELETE")
-//                    .allowedHeaders("*")
-//                    .allowedOrigins("*");
-//            }
-//
-//        };
-//    }
-//}
+package com.springboysspring.simplynotes.configuration;
+
+import javax.validation.constraints.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfiguration {
+
+    @NotNull
+    @Bean
+    public WebMvcConfigurer CORSConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*")
+                    .allowedOrigins("*");
+            }
+
+        };
+    }
+}
